8d888ec7a189d6d75c01f9ab7043c711455acfc6
David Doan <daviddoan@Davids-MacBook-Pro.local>
2025-03-18 11:21:15 -0600

    solution

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [state, setState] = useState(localStorage.getItem(key) || initialValue);
+
+    useEffect(() => {
+      if (state !== null) {
+        localStorage.setItem(key, state);
+      }
+    }, [key, state]);
+  
+    return [state, setState];
 }
